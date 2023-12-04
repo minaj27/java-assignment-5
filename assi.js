f2de2c33e8587b1977d07de7c4ae716640bf73d4
minaj27 <minaj13m@gmail.com>
2023-12-05 00:07:32 +0530

    first commit

---
+++ assi.js
@@ -0,0 +1,54 @@
+document.write("<h2>Arithmetic Operators</h2>");
+var num1=30;
+var num2=10;
+var sum=num1+num2;
+document.write("<b>Addition + =</b>");
+document.write("<b>"+sum+"</b>");
+document.write("<br>");
+document.write("<br>");
+
+var sub=num1-num2;
+document.write("<b>Subtraction - =</b>");
+document.write("<b>"+sub+"</b>");
+document.write("<br>");
+document.write("<br>");
+
+var multi=num1*num2;
+document.write("<b>Multiplication * =</b>");
+document.write("<b>"+multi+"</b>");
+document.write("<br>");
+document.write("<br>");
+
+var div=num1/num2;
+document.write("<b>Division / =</b>");
+document.write("<b>"+div+"</b>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<hr>");
+document.write("<hr>");
+
+document.write("<h2>Small Senternces</h2>");
+var fname="Salman";
+var lname="Khan";
+
+document.write("<b>First Name ="+ fname +"</b>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<b>Last Name ="+lname+"</b>");
+document.write("<br>");
+document.write("<br>");
+
+document.write("<b>*"+fname+lname+"always has a smile on his face.</b>")
+document.write("<br>");
+document.write("<br>");
+document.write("<b>*"+fname+lname+"artwork is truly impressive.</b>")
+document.write("<br>");
+document.write("<br>");
+document.write("<b>* I had a great conversation with"+fname+lname+"yesterday.</b>")
+document.write("<br>");
+document.write("<br>");
+document.write("<b>* Do you know"+fname+lname+"? His really smart.</b>")
+
+
